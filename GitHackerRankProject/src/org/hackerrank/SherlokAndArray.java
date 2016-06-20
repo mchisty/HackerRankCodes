2be32c912870507815b4f7ecb389ec5935689898
M Chisty <mchisty@gmail.com>
2016-06-20 22:03:18 +1000

    Commited SherlockAndArray

--- GitHackerRankProject/src/org/hackerrank/SherlokAndArray.java
+++ GitHackerRankProject/src/org/hackerrank/SherlokAndArray.java
@@ -1,8 +1,6 @@
 package org.hackerrank;
 // Sample input
 
-import java.util.Scanner;
-
 //2
 //3
 //1 2 3
@@ -15,19 +13,19 @@ import java.util.Scanner;
 //--------------------
 public class SherlokAndArray {
     public static void main(String[] args) {
-	Scanner in = new Scanner(System.in);
-	int t = in.nextInt();
-	for (int x = 0; x < t; x++) {
-	    int size = in.nextInt();
-	    int[] a = new int[size];
-	    for (int i = 0; i < size; ++i) {
-		a[i] = in.nextInt();
-	    }
-	    System.out.println(getResult(a));
-	}
-	in.close();
-	// int[] b = { 1, 1, 1, 1, 2, 2 };
-	// System.out.println(getResult(b));
+	// Scanner in = new Scanner(System.in);
+	// int t = in.nextInt();
+	// for (int x = 0; x < t; x++) {
+	// int size = in.nextInt();
+	// int[] a = new int[size];
+	// for (int i = 0; i < size; ++i) {
+	// a[i] = in.nextInt();
+	// }
+	// System.out.println(getResult(a));
+	// }
+	// in.close();
+	int[] b = { 1, 2, 3 };
+	System.out.println(getResult(b));
     }
 
     static String getResult(int[] a) {
@@ -35,28 +33,29 @@ public class SherlokAndArray {
 	    return "YES";
 	}
 	boolean flag = false;
-	for (int i = 1; i < a.length; ++i) {
-	    flag = isEqual(a, i);
-	    if (flag) {
-		break;
-	    }
-	}
-	// flag = isEqual(a, 2);
+	flag = isEqual(a);
 	return flag ? "YES" : "NO";
     }
 
-    static boolean isEqual(int[] a, int midPos) {
-	int sumLeft = 0;
+    static boolean isEqual(int[] a) {
 	int sumRight = 0;
-	for (int i = 0, j = midPos + 1; i < midPos || j < a.length; ++i, ++j) {
-	    if (i < midPos) {
+	boolean flag = false;
+	for (int i = 1; i < a.length; ++i) {
+	    sumRight = sumRight + a[i];
+	}
+	int sumLeft = 0;
+	for (int i = 0; i < a.length; ++i) {
+	    if (i + 1 < a.length) {
+		sumRight = sumRight - a[i + 1];
+	    }
+	    if (i >= 0) {
 		sumLeft = sumLeft + a[i];
 	    }
-	    if (j < a.length) {
-		sumRight = sumRight + a[j];
-		// System.out.println(a[j] + " = " + sumRight);
+	    if (sumLeft == sumRight) {
+		flag = true;
+		break;
 	    }
 	}
-	return sumLeft == sumRight;
+	return flag;
     }
 }

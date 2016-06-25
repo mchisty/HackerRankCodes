3ed7ce91d2bce04f8d1b97b03bf6f9fea77c9b04
M Chisty <mchisty@gmail.com>
2016-06-25 20:22:10 +1000

    ArgoMatrix commited

--- GitHackerRankProject/src/org/hackerrank/ArgoMatrixRotation.java
+++ GitHackerRankProject/src/org/hackerrank/ArgoMatrixRotation.java
@@ -1,176 +1,270 @@
 package org.hackerrank;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStreamReader;
-import java.util.Scanner;
-import java.util.StringTokenizer;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
 
+// Works fine, just timeout issue:
 public class ArgoMatrixRotation {
+    // int ROWS = 0, COLS = 0;
+    // int ROTATIONS = 0;
+    // int tmp[][] = null;
+    // int a[][] = null;
+    final static String TOP_TO_BOTTOM = "LEFT";
+    final static String LEFT_TO_RIGHT = "BOTTOM";
+    final static String BOTTOM_TO_TOP = "RIGHT";
+    final static String RIGHT_TO_LEFT = "TOP";
+    Map<String, List<Integer>> map = new HashMap<>();
 
-	int ROWS = 0, COLS = 0;
-	int ROTATIONS = 0;
-	int tmp[][] = null;
-	int a[][] = null;
+    public ArgoMatrixRotation() {
+    }
 
-	public ArgoMatrixRotation() {
+    // Row associates X direction. row = X
+    // Column associates Y direction. col = Y
+    /**
+     * ----------------- > Y <br/>
+     * 
+     * | <br/>
+     * | <br/>
+     * | <br/>
+     * | <br/>
+     * | <br/>
+     * | <br/>
+     * 
+     * 
+     * \|/
+     * 
+     * X
+     * 
+     */
+    public static void main(String[] args) {
+	ArgoMatrixRotation mt = new ArgoMatrixRotation();
+	// // mt.takeInput(in);
+	// mt.readInputFromFile();
+	// in.close();
+	// mt.calculate();
+	int startY = 0;
+	int startX = 0;
+	int row = 10;
+	int col = 8;
+	int numberOfRotations = 40;
+	int[][] a = new int[row][col];
+	Random r = new Random();
+	// populate sample data
+	for (int x = 0; x < row; ++x) {
+	    for (int y = 0; y < col; ++y) {
+		a[x][y] = r.nextInt(1000) + r.nextInt(500);
+	    }
 	}
-
-	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
-		ArgoMatrixRotation mt = new ArgoMatrixRotation();
-		// mt.takeInput(in);
-		mt.readInputFromFile();
-		in.close();
-		mt.calculate();
+	System.out.println("========================== Sample array ======================");
+	for (int x = 0; x < row; ++x) {
+	    for (int y = 0; y < col; ++y) {
+		System.out.print("\t" + a[x][y]);
+	    }
+	    System.out.println();
 	}
-
-	void takeInput(Scanner in) {
-		ROWS = in.nextInt();
-		COLS = in.nextInt();
-		ROTATIONS = in.nextInt();
-		a = new int[ROWS][COLS];
-		tmp = new int[ROWS][COLS];
-		for (int i = 0; i < ROWS; ++i) {
-			for (int j = 0; j < COLS; ++j) {
-				a[i][j] = in.nextInt();
-			}
-		}
-		updateNumberofRotations();
+	System.out.println("==============================================================");
+	// numberOfRotations = mt.getActualNumberOfRotations(row, col, numberOfRotations);
+	int fullRotation = (row - 1) * 2 + (col - 1) * 2;
+	System.out.println("number of max rotations (outer boundary):" + fullRotation);
+	for (int n = 0; n < numberOfRotations; ++n) {
+	    mt.performOneShift(a, startX, startY, row, col);
 	}
-
-	void readInputFromFile() {
-		try {
-			File file = new File("C:\\Temp\\matrix_input01.txt");
-			FileInputStream fin = new FileInputStream(file);
-			InputStreamReader iread = new InputStreamReader(fin);
-			BufferedReader br = new BufferedReader(iread);
-
-			String line1 = br.readLine();
-			StringTokenizer stoken = new StringTokenizer(line1, "\t ");
-			ROWS = Integer.parseInt(stoken.nextToken());
-			COLS = Integer.parseInt(stoken.nextToken());
-			ROTATIONS = Integer.parseInt(stoken.nextToken());
-			a = new int[ROWS][COLS];
-			tmp = new int[ROWS][COLS];
-
-			for (int i = 0; i < ROWS; ++i) {
-				String line2 = br.readLine();
-				populateArray(line2, i);
-			}
-
-			// ----------------------------
-			// for (int i = 0; i < ROWS; ++i) {
-			// for (int j = 0; j < COLS; ++j) {
-			// System.out.print(a[i][j] + " ");
-			// }
-			// System.out.println();
-			// }
-
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-
+	for (int x = 0; x < row; ++x) {
+	    for (int y = 0; y < col; ++y) {
+		System.out.print("\t" + a[x][y]);
+	    }
+	    System.out.println();
 	}
-
-	void populateArray(String line, int row) {
-		StringTokenizer stoken = new StringTokenizer(line, "\t ");
-		int j = 0;
-		while (stoken.hasMoreTokens()) {
-			a[row][j] = Integer.parseInt(stoken.nextToken());
-			++j;
-		}
-	}
-
-	void updateNumberofRotations() {
-		int I = 2 * ROWS + 2 * COLS - 4;
-		if (ROTATIONS > I) {
-			ROTATIONS = ROTATIONS % I;
-		}
-	}
-
-	void calculate() {
-		for (int count = 0; count < ROTATIONS; ++count) {
-			for (int startpos = 0, row1 = 0, col1 = 0; row1 < ROWS && col1 < COLS; ++row1, ++col1, ++startpos) {
-				rotate(startpos, row1, col1, ROWS - row1, COLS - col1);
-			}
-			a = tmp;
-			tmp = new int[ROWS][COLS];
-			for (int i = 0; i < ROWS; ++i) {
-				for (int j = 0; j < COLS; ++j) {
-					if (count == ROTATIONS - 1) {
-						System.out.print(a[i][j] + " ");
-					}
-				}
-				if (count == ROTATIONS - 1) {
-					System.out.println();
-				}
-			}
-		}
-	}
-
-	// void rotate(final int startpos, int i, int j, int numOfRows, int numOfCols) {
-	// moveTopToBottom(i, j, numOfRows, numOfCols);
-	// moveLeftToRight(numOfRows - 1, j, numOfRows, numOfCols);
-	// moveBottomToTop(startpos, numOfRows - 1, numOfCols - 1, numOfRows, numOfCols);
-	// moveRightToLeft(startpos, i, numOfCols - 1, numOfRows, numOfCols);
+	// ========================================================
+	// Actual Scanner input
+	// Scanner in = new Scanner(System.in);
+	// int row = in.nextInt();
+	// int col = in.nextInt();
+	// int numberOfRotations = in.nextInt();
+	// int[][] a = new int[row][col];
+	// // input sample data
+	// for (int x = 0; x < row; ++x) {
+	// for (int y = 0; y < col; ++y) {
+	// a[x][y] = in.nextInt();
 	// }
+	// }
+	// numberOfRotations = mt.getActualNumberOfRotations(row, col, numberOfRotations);
+	// for (int n = 0; n < numberOfRotations; ++n) {
+	// mt.performOneShift(a, 0, 0, row, col);
+	// }
+	// for (int x = 0; x < row; ++x) {
+	// for (int y = 0; y < col; ++y) {
+	// System.out.print(a[x][y] + " ");
+	// }
+	// System.out.println();
+	// }
+    }
 
-	void rotate(final int startpos, int i, int j, final int numOfRows, final int numOfCols) {
-		int endX = numOfRows - i - 1;
-		int endY = numOfCols - j - 1;
-		if (i < endX && j < endY) {
-			moveTopToBottom(i, j, numOfRows, numOfCols);
-		}
-		// ----------------------------------------------------------
-		i = numOfRows - 1;
-		endX = numOfRows - i - 1;
-		if (i > endX && j < endY) {
-			moveLeftToRight(i, j, numOfRows, numOfCols);
-		}
-		// ----------------------------------------------------------
-		j = numOfCols - 1;
-		endY = numOfCols - j - 1;
-		if (i > endX && j > endY) {
-			moveBottomToTop(startpos, i, j, numOfRows, numOfCols);
-		}
-		// ----------------------------------------------------------
-		if (i < endX && j > endY) {
-			moveRightToLeft(startpos, i, j, numOfRows, numOfCols);
-		}
+    int getActualNumberOfRotations(int row, int col, int numberOfRoations) {
+	int fullRotation = (row - 1) * 2 + (col - 1) * 2;
+	if (numberOfRoations > fullRotation) {
+	    numberOfRoations = numberOfRoations % fullRotation;
 	}
+	return numberOfRoations;
+    }
 
-	void moveTopToBottom(int i, int j, int numOfRows, int numOfCols) {
-		if (i < numOfRows - 1) {
-			int i1 = i + 1;
-			tmp[i1][j] = a[i][j];
-			moveTopToBottom(i1, j, numOfRows, numOfCols);
-		}
+    private void performOneShift(int[][] a, int startX, int startY, int row, int col) {
+	if (isOneShiftValidForCurrentPosition(startX, startY, row, col)) {
+	    // Top to bottom
+	    List<Integer> topToBottom = map.get(TOP_TO_BOTTOM);
+	    int[] topToBottomOriginal = new int[topToBottom.size()];
+	    int k = 0;
+	    for (Integer i : topToBottom) {
+		topToBottomOriginal[k++] = a[i - 1][startY];
+	    }
+	    // Left to right
+	    List<Integer> leftToRight = map.get(LEFT_TO_RIGHT);
+	    int[] leftToRightOriginal = new int[leftToRight.size()];
+	    k = 0;
+	    for (Integer i : leftToRight) {
+		leftToRightOriginal[k++] = a[row - 1][i - 1];
+	    }
+	    // Bottom to top
+	    List<Integer> bottomToTop = map.get(BOTTOM_TO_TOP);
+	    int[] bottomToTopOriginal = new int[bottomToTop.size()];
+	    k = 0;
+	    for (Integer i : bottomToTop) {
+		bottomToTopOriginal[k++] = a[i + 1][col - 1];
+	    }
+	    // Right to left
+	    List<Integer> rightToLeft = map.get(RIGHT_TO_LEFT);
+	    int[] rightToLeftOriginal = new int[rightToLeft.size()];
+	    k = 0;
+	    for (Integer i : rightToLeft) {
+		rightToLeftOriginal[k++] = a[startX][i + 1];
+	    }
+	    // Update the values in a[][]
+	    // top to bottom
+	    k = 0;
+	    for (Integer i : topToBottom) {
+		a[i][startY] = topToBottomOriginal[k++];
+	    }
+	    // left to right
+	    k = 0;
+	    for (Integer i : leftToRight) {
+		a[row - 1][i] = leftToRightOriginal[k++];
+	    }
+	    // bottom to top
+	    k = 0;
+	    for (Integer i : bottomToTop) {
+		a[i][col - 1] = bottomToTopOriginal[k++];
+	    }
+	    // right to left
+	    k = 0;
+	    for (Integer i : rightToLeft) {
+		a[startX][i] = rightToLeftOriginal[k++];
+	    }
+	    startX = startX + 1;
+	    startY = startY + 1;
+	    row = row - 1;
+	    col = col - 1;
+	    performOneShift(a, startX, startY, row, col);
 	}
+    }
 
-	void moveLeftToRight(int i, int j, int numOfRows, int numOfCols) {
-		if (j < numOfCols - 1) {
-			int j1 = j + 1;
-			tmp[i][j1] = a[i][j];
-			moveLeftToRight(i, j1, numOfRows, numOfCols);
-		}
+    /**
+     * The idea here is to check if, after shifting cell one position, it matches with one of the list items. If so,
+     * then do the shifting; otherwise don't.
+     */
+    boolean isOneShiftValidForCurrentPosition(int currentX, int currentY, int expectedRow, int expectedCol) {
+	map = getAllValidInnerPositions(currentX, currentY, expectedRow, expectedCol);
+	if (map.get(TOP_TO_BOTTOM).isEmpty()) {
+	    return false;
+	} else if (map.get(LEFT_TO_RIGHT).isEmpty()) {
+	    return false;
+	} else if (map.get(BOTTOM_TO_TOP).isEmpty()) {
+	    return false;
+	} else if (map.get(RIGHT_TO_LEFT).isEmpty()) {
+	    return false;
 	}
+	return true;
+    }
 
-	void moveBottomToTop(final int startpos, int i, int j, int numOfRows, int numOfCols) {
-		if (i > startpos) {
-			int i1 = i - 1;
-			tmp[i1][j] = a[i][j];
-			moveBottomToTop(startpos, i1, j, numOfRows, numOfCols);
-		}
+    Map<String, List<Integer>> getAllValidInnerPositions(int currentX, int currentY, int expectedRow, int expectedCol) {
+	map.clear();
+	// Top to bottom
+	List<Integer> a = new ArrayList<>();
+	for (int i = currentX + 1; i < expectedRow; ++i) {
+	    a.add(i);
 	}
-
-	void moveRightToLeft(final int startpos, int i, int j, int numOfRows, int numOfCols) {
-		if (j > startpos) {
-			int j1 = j - 1;
-			tmp[i][j1] = a[i][j];
-			moveRightToLeft(startpos, i, j1, numOfRows, numOfCols);
-		}
+	map.put(TOP_TO_BOTTOM, a);
+	// Left to right
+	a = new ArrayList<>();
+	for (int i = currentY + 1; i < expectedCol; ++i) {
+	    a.add(i);
 	}
-
+	map.put(LEFT_TO_RIGHT, a);
+	// Bottom to top
+	a = new ArrayList<>();
+	for (int i = expectedRow - 2; i >= currentX; --i) {
+	    a.add(i);
+	}
+	map.put(BOTTOM_TO_TOP, a);
+	// Right to left
+	a = new ArrayList<>();
+	for (int i = expectedCol - 2; i >= currentY; --i) {
+	    a.add(i);
+	}
+	map.put(RIGHT_TO_LEFT, a);
+	return map;
+    }
+    // ========================================================================================================
+    // void takeInput(Scanner in) {
+    // ROWS = in.nextInt();
+    // COLS = in.nextInt();
+    // ROTATIONS = in.nextInt();
+    // a = new int[ROWS][COLS];
+    // tmp = new int[ROWS][COLS];
+    // for (int i = 0; i < ROWS; ++i) {
+    // for (int j = 0; j < COLS; ++j) {
+    // a[i][j] = in.nextInt();
+    // }
+    // }
+    // }
+    //
+    // void readInputFromFile() {
+    // try {
+    // File file = new File("C:\\Temp\\matrix_input01.txt");
+    // FileInputStream fin = new FileInputStream(file);
+    // InputStreamReader iread = new InputStreamReader(fin);
+    // BufferedReader br = new BufferedReader(iread);
+    // String line1 = br.readLine();
+    // StringTokenizer stoken = new StringTokenizer(line1, "\t ");
+    // ROWS = Integer.parseInt(stoken.nextToken());
+    // COLS = Integer.parseInt(stoken.nextToken());
+    // ROTATIONS = Integer.parseInt(stoken.nextToken());
+    // a = new int[ROWS][COLS];
+    // tmp = new int[ROWS][COLS];
+    // for (int i = 0; i < ROWS; ++i) {
+    // String line2 = br.readLine();
+    // populateArray(line2, i);
+    // }
+    // // ----------------------------
+    // // for (int i = 0; i < ROWS; ++i) {
+    // // for (int j = 0; j < COLS; ++j) {
+    // // System.out.print(a[i][j] + " ");
+    // // }
+    // // System.out.println();
+    // // }
+    // } catch (Exception e) {
+    // e.printStackTrace();
+    // }
+    // }
+    //
+    // void populateArray(String line, int row) {
+    // StringTokenizer stoken = new StringTokenizer(line, "\t ");
+    // int j = 0;
+    // while (stoken.hasMoreTokens()) {
+    // a[row][j] = Integer.parseInt(stoken.nextToken());
+    // ++j;
+    // }
+    // }
 }

925059f81b63773707c3596dd34f8e4abfa8b734
M M Islam Chisty <mchisty@gmail.com>
2016-09-30 15:43:59 +1000

    Code updated

--- GitHackerRankProject/src/misc/crackingcode/HashTableRansomNote.java
+++ GitHackerRankProject/src/misc/crackingcode/HashTableRansomNote.java
@@ -2,89 +2,58 @@ package misc.crackingcode;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.StringTokenizer;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class HashTableRansomNote {
 
-	Map<String, Integer> magazineMap;
-	// Map<String, Integer> noteMap;
-
 	public static void main(String[] args) {
-		// String magazine = "give me one grand today night";
-		// String note = "give one grand today";
-		// HashTableRansomNote h = new HashTableRansomNote();
-		// System.out.println(h.solve(magazine, note));
+		// String[] magazine = { "give", "me", "one", "grand", "today", "night"
+		// };
+		// String[] note = { "give", "one", "grand", "today" };
+		// Expected: Yes
+		// -------------------------------------------------------------
 
-		String[] magazine = { "give", "me", "one", "grand", "today", "night" };
-		String[] note = { "give", "one", "grand", "today" };
+		String one = "o l x imjaw bee khmla v o v o imjaw l khmla imjaw x";
+		String two = "imjaw l khmla x imjaw o l l o khmla v bee o o imjaw imjaw o";
+		String[] magazine = one.split("\\s+");
+		String[] note = two.split("\\s+");
+		// Expected: No
+		// -------------------------------------------------------------
+		// String[] magazine = { "give", "me" };
+		// String[] note = { "give", "me", "give", "me" };
+		// Expected: Yes
+
 		HashTableRansomNote h = new HashTableRansomNote();
 		String result = h.solve(magazine, note) ? "Yes" : "No";
 		System.out.println(result);
 
 	}
 
-	// public HashTableRansomNote(String magazine, String note) {
-	// magazineMap = new HashMap<>();
-	// noteMap = new HashMap<>();
-	// // TODO
-	// StringTokenizer stoken = new StringTokenizer(magazine, " \t");
-	// int count = 0;
-	// while (stoken.hasMoreTokens()) {
-	// String key = stoken.nextToken();
-	// if (magazineMap.containsKey(key)) {
-	// count = magazineMap.get(key) + 1;
-	// } else {
-	// count = 0;
-	// }
-	// ++count;
-	// magazineMap.put(key, count);
-	// }
-
-	// }
-
-	public boolean solve(String magazine, String note) {
-		magazineMap = new HashMap<>();
-		StringTokenizer stoken = new StringTokenizer(magazine, " \t");
+	public boolean solve(String[] magazine, String[] ransom) {
 		int count = 0;
-		while (stoken.hasMoreTokens()) {
-			String key = stoken.nextToken();
-			if (magazineMap.containsKey(key)) {
-				count = magazineMap.get(key) + 1;
+		Map<String, Integer> magazineMap = new HashMap<>(30000);
+		for (String s : magazine) {
+			if (magazineMap.containsKey(s)) {
+				count = magazineMap.get(s) + 1;
 			} else {
 				count = 0;
 			}
 			++count;
-			magazineMap.put(key, count);
+			magazineMap.put(s, count);
 		}
 
-		stoken = new StringTokenizer(note, " \t");
-		while (stoken.hasMoreTokens()) {
-			String key = stoken.nextToken();
-			if (!magazineMap.containsKey(key)) {
-				return false;
-			}
-		}
-
-		return true;
-
-	}
-
-	// Using array
-
-	public boolean solve(String[] magazine, String[] note) {
-		int count = 1;
-		Stream<String> words = Stream.of(magazine);
-		magazineMap = words.collect(Collectors.toMap(k -> k, v -> count));
-		for (String s : note) {
+		for (String s : ransom) {
 			if (!magazineMap.containsKey(s)) {
 				return false;
 			}
+			count = magazineMap.get(s);
+			if (count == 0) {
+				return false;
+			} else {
+				count = count - 1;
+				magazineMap.put(s, count);
+			}
 		}
-		// magazineMap.forEach((k, v) -> System.out.println(k + " " + v));
 		return true;
-
 	}
 
 }

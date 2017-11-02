68bd1854be4d923cadd20a22f5cbf8dedc2e96ff
M M Islam Chisty <mchisty@gmail.com>
2017-11-03 09:06:29 +1100

    Javadoc added

--- GitHackerRankProject/src/misc/sorting/QuickSortExample2.java
+++ GitHackerRankProject/src/misc/sorting/QuickSortExample2.java
@@ -3,6 +3,7 @@ package misc.sorting;
 
 import java.util.stream.IntStream;
 
+// TODO: Auto-generated Javadoc
 /**
  * This approach takes middle element as pivot. The best case is O(n*log n) when
  * middle element is chosen as pivot
@@ -12,50 +13,74 @@ import java.util.stream.IntStream;
  */
 public class QuickSortExample2 {
 
-	static int[] a = { 10, 80, 30, 85, 90, 60, 50, 70 };
+	/** The a. */
+	static int[] a = { 90, 10, 80, 20, 85, 40 };
 
+	/**
+	 * The main method.
+	 *
+	 * @param args
+	 *            the arguments
+	 */
 	public static void main(String[] args) {
 		System.out.print("Initial value => ");
 		IntStream.of(a).forEach(k -> System.out.print(" " + k));
 		System.out.println();
 		QuickSortExample2 q = new QuickSortExample2();
 		q.quickSort(0, a.length - 1);
+		System.out.print("Final value => ");
 		IntStream.of(a).forEach(i -> System.out.print(" " + i));
 	}
 
+	/**
+	 * Quick sort.
+	 *
+	 * @param low
+	 *            the low
+	 * @param high
+	 *            the high
+	 */
 	public void quickSort(int low, int high) {
-		int i = low, j = high;
+		int leftPos = low, rightPos = high;
 		int pivotPos = (low + high) / 2;
 		int pivot = a[pivotPos];
-		System.out.println("-------------------------------------------------");
-		System.out.println("Pivot pos: " + pivotPos + " ; Pivot val: " + pivot);
-		while (i <= j) {
-			while (a[i] < pivot) {
-				i++;
+		while (leftPos < rightPos) {
+			while (a[leftPos] < pivot) {
+				leftPos++;
 			}
-			while (a[j] > pivot) {
-				j--;
+			while (a[rightPos] > pivot) {
+				rightPos--;
 			}
-			if (i <= j) {
-				System.out.println("i:" + i + " a[i]: " + a[i] + "; j:" + j + " a[j]: " + a[j]);
-				swap(i, j);
-				i++;
-				j--;
-				System.out.print("After swapping => ");
-				IntStream.of(a).forEach(k -> System.out.print(" " + k));
-				System.out.println();
+			if (leftPos <= rightPos) {
+				System.out.println("leftPos:" + leftPos + " a[leftPos]: " + a[leftPos] + " <=======> rightPos:"
+						+ rightPos + " a[rightPos]: " + a[rightPos] + ". high: " + high + ", low: " + low);
+				if (leftPos < rightPos) {
+					swap(leftPos, rightPos);
+				}
+				leftPos++;
+				rightPos--;
 			}
 		}
-		if (i < high) {
-			System.out.println("i: " + i + "; high: " + high);
-			quickSort(i, high);
+		if (leftPos < high) {
+			// System.out.println("\t---> left pos: " + leftPos + "; high: " +
+			// high);
+			quickSort(leftPos, high);
 		}
-		if (j > low) {
-			System.out.println("low: " + low + "; j: " + j);
-			quickSort(low, j);
+		if (rightPos > low) {
+			// System.out.println("\t---> right pos: " + rightPos + "; low: " +
+			// low);
+			quickSort(low, rightPos);
 		}
 	}
 
+	/**
+	 * Swap.
+	 *
+	 * @param i
+	 *            the i
+	 * @param j
+	 *            the j
+	 */
 	void swap(int i, int j) {
 		int tmp = a[i];
 		a[i] = a[j];

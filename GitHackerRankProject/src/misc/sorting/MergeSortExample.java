38d3d30cd226905e9b5833cf7c6104efe77e144c
M M Islam Chisty <mchisty@gmail.com>
2018-02-08 15:15:13 +1100

    From office

--- GitHackerRankProject/src/misc/sorting/MergeSortExample.java
+++ GitHackerRankProject/src/misc/sorting/MergeSortExample.java
@@ -1,19 +1,20 @@
+
 package misc.sorting;
 
 public class MergeSortExample {
 
-	static int[] a = { 90, 20, 85, 40 }; // { 90, 10, 80, 20, 85, 40 };
-	static int[] c = a; // new int[a.length];
+	static int[] initialArray = { 90, 11, 85, 101 };
+
+	static int[] c = initialArray; // new int[a.length];
 
 	public static void main(String[] args) {
 		MergeSortExample ms = new MergeSortExample();
-		// ms.mergeSort(0, a.length - 1);
-		// c = ms.divideAndConquer(a);
-		c = ms.divide1(a);
-
-		for (int i = 0; i < c.length; ++i)
+		c = ms.divideAndConquer(initialArray);
+		// -------------------------------------
+		// c = ms.divide1(initialArray);
+		for (int i = 0; i < c.length; ++i) {
 			System.out.print(c[i] + " ");
-
+		}
 	}
 
 	public int[] divideAndConquer(int[] inputArray) {
@@ -28,42 +29,11 @@ public class MergeSortExample {
 		System.arraycopy(inputArray, leftArray.length, rightArray, 0, rightArray.length);
 		divideAndConquer(leftArray);
 		divideAndConquer(rightArray);
-		merge(leftArray, rightArray, inputArray);
+		sort1(leftArray, rightArray, inputArray);
 		return inputArray;
 	}
 
-	public void merge(int[] leftArray, int[] rightArray, int[] sortedArray) {
-		int leftArrayLength = leftArray.length;
-		int rightArrayLength = rightArray.length;
-		int i = 0;
-		int j = 0;
-		int k = 0;
-		while (i < leftArrayLength && j < rightArrayLength) {
-			if (leftArray[i] < rightArray[j]) {
-				sortedArray[k] = leftArray[i];
-				i++;
-			} else {
-				sortedArray[k] = rightArray[j];
-				j++;
-			}
-			k++;
-		}
-		// copy the rest of the first half if there is anything left
-		while (i < leftArrayLength) {
-			sortedArray[k] = leftArray[i];
-			i++;
-			k++;
-		}
-		// copy the rest of the second half if there is anything left
-		while (j < rightArrayLength) {
-			sortedArray[k] = rightArray[j];
-			j++;
-			k++;
-		}
-	}
-
 	// ===============================================================
-
 	private int[] divide1(int[] array) {
 		int n = array.length;
 		if (n == 1) {
@@ -72,40 +42,34 @@ public class MergeSortExample {
 		int mid = n / 2;
 		int[] a = new int[mid];
 		int[] b = new int[n - mid];
-
 		System.arraycopy(array, 0, a, 0, a.length);
 		System.arraycopy(array, a.length, b, 0, b.length);
-
 		divide1(a);
 		divide1(b);
-		merge(a, b, array);
+		// merge(a, b, array);
+		sort1(a, b, array);
 		return array;
 	}
 
-	public void sort1(int[] a, int[] b, int[] tmp) {
-		int leftSize = a.length;
-		int rightSize = b.length;
+	public void sort1(int[] a1, int[] b1, int[] tmp) {
+		int leftSize = a1.length;
+		int rightSize = b1.length;
 		int i = 0, j = 0, k = 0;
-
 		while (i < leftSize && j < rightSize) {
-			if (a[i] <= b[j]) {
-				tmp[k] = a[i];
-				++i;
+			if (a1[i] < b1[j]) {
+				tmp[k] = a1[i];
+				i++;
 			} else {
-				tmp[k] = b[j];
-				++j;
+				tmp[k] = b1[j];
+				j++;
 			}
-			++k;
+			k++;
 		}
-
-		while (i <= leftSize) {
-			tmp[k++] = a[i++];
+		while (i < leftSize) {
+			tmp[k++] = a1[i++];
 		}
-
-		while (j <= rightSize) {
-			tmp[k++] = a[j++];
+		while (j < rightSize) {
+			tmp[k++] = b1[j++];
 		}
-
 	}
-
 }

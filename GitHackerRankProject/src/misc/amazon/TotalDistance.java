aa46ba97b5df17d3c5f2dd2d7f5a94ad00f74532
M Chisty <mchisty@gmail.com>
2020-02-17 21:51:18 +1100

    File updated

--- GitHackerRankProject/src/misc/amazon/TotalDistance.java
+++ GitHackerRankProject/src/misc/amazon/TotalDistance.java
@@ -7,30 +7,41 @@ import java.util.List;
 public class TotalDistance {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		int nuRows = 3;
 		int numColumns = 3;
 		List<List<Integer>> area = new ArrayList<List<Integer>>(
-				Arrays.asList(Arrays.asList(1, 0, 0), Arrays.asList(1, 0, 0), Arrays.asList(1, 0, 0)));
+//				Arrays.asList(Arrays.asList(1, 0, 0), Arrays.asList(1, 0, 0), Arrays.asList(1, 9, 1)));
+				Arrays.asList(Arrays.asList(1, 1, 0), Arrays.asList(0, 1, 1), Arrays.asList(0, 1, 9)));
 
 		System.out.println(minimumDistance(nuRows, numColumns, area));
 	}
 
 	static int minimumDistance(int nuRows, int numColumns, List<List<Integer>> area) {
 		int count = 0;
-		// Scan row-by-row
-		outer: for (List<Integer> rowList : area) {
-			// Scan cell
-			inner: for (Integer cell : rowList) {
+		int activePos = 0;
+		outer: for (int i = 0; i < area.size(); ++i) {
+			List<Integer> row = area.get(i);
+			inner: for (int j = activePos; j < row.size(); ++j) {
+				int cell = row.get(j);
 				if (cell == 1) {
-					count++;
-				} else {
+					activePos = j;
+					System.out.println("i:" + i + ", j: " + activePos + ", cell value:" + cell);
+					++count;
+
+				} else if (cell == 9) {
+					activePos = j;
+					System.out.println("i:" + i + ", j: " + activePos + ", cell value:" + cell);
+					break outer;
+				}
+				else {
 					break inner;
+
 				}
 			}
 		}
 
 		return count;
 	}
+
 
 }
